5424ce1118a02aa99fcabd4631a1e3a3c0ba9779
Aman Tank <amantank187@gmail.com>
2021-01-12 15:55:18 +0000

    Added plane unit test and passed it

--- airport_js/jasmine-standalone-3.6.0/src/plane.js
+++ airport_js/jasmine-standalone-3.6.0/src/plane.js
@@ -1,13 +1,5 @@
+'use strict';
 
 class Plane {
-  constructor(landed = true){
-    this.plane_status = landed;
-  }
-  land(){
-    this.plane_status = true
-  };
-
-  takeoff(){
-    this.plane_status = false
-  };
+    land(){};
 }
